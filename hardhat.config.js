5652511770b3be55c2c5ac922d6c8b6e3cb1b65b
Santiago Diaz <santidiaz.uy@gmail.com>
2021-11-03 21:40:39 -0300

    Configuracion para deploy con hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  defaultNetwork: "rinkeby", // <ganache> por defecto.
+  networks: {
+    hardhat: {
+      chainid: 31337
+    },
+    ganache: {
+      chainid: 1337,
+      url: process.env.GANACHE_URL,
+      accounts: [process.env.PRIVATE_KEY_G],
+      from: process.env.ACCOUNT_ADDRESS_G
+    },
+    rinkeby: {
+      chainid: 4,
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY_R],
+      from: process.env.ACCOUNT_ADDRESS_R
+    }
+  }
 };
