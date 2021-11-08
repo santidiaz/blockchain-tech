3f9bc67acde827dd9f0f4c8256fb42d7ce812e6f
Santiago Diaz <santidiaz.uy@gmail.com>
2021-11-08 12:53:14 -0300

    Deploy js pequeños cambios

--- scripts/deploy_01.js
+++ scripts/deploy_01.js
@@ -1,17 +1,17 @@
 const { ethers } = require("hardhat");
 
-// Ejecutar deploy con: [npx hardhat run scripts/deploy_01.js --network Rinkeby]
+// Ejecutar deploy con: [npx hardhat run scripts/deploy_01.js --network rinkeby]
 async function main() {
     console.log("=> Deploy started...");
 
     const deployer = await ethers.getSigner();
     console.log("=> Deployer account address: ", deployer.address);
-    console.log("=> Deployer account balance: ", ethers.utils.formatEther(await deployer.getBalance()));
+    console.log("=> Address balance before deploy: ", ethers.utils.formatEther(await deployer.getBalance()));
 
     const contractFactory = await ethers.getContractFactory("SmartInvestment");
     const contractInstance = await contractFactory.deploy();
     console.log("=> Contract deployed to address: ", contractInstance.address);
-    console.log("-- Deploy account balance: ", ethers.utils.formatEther(await deployer.getBalance()));
+    console.log("-- Address balance after deploy: ", ethers.utils.formatEther(await deployer.getBalance()));
     console.log("-- Deploy finished");
 }
 
