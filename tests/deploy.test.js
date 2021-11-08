68897a4615da11abe0f7b003c811ec3ad819c0a3
Santiago Diaz <santidiaz.uy@gmail.com>
2021-11-08 12:53:43 -0300

    Tests del constructor de SmartInvestment y AddOwner

--- tests/deploy.test.js
+++ tests/deploy.test.js
@@ -1,17 +1,19 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 let contractInstance;
+let deployer;
 
+// Ejecutar con: [npx hardhat test tests/deploy.test.js]
 before(async function() {
     console.log("-- Deploy started");
 
-    const deployer = await ethers.getSigner(); // Obtiene la primera cuenta en mi nodo local
+    deployer = await ethers.getSigner(); // Obtiene la primera cuenta en mi nodo local
     console.log("-- Deployer account address: ", deployer.address);
     console.log("-- Deploy account balance: ", ethers.utils.formatEther(await deployer.getBalance()));
 
-    const contractFactory = await ethers.getContractFactory("MyDeployContract");
-    const contractInstance = await contractFactory.deploy();
+    const contractFactory = await ethers.getContractFactory("SmartInvestment");
+    contractInstance = await contractFactory.deploy();
     console.log("-- Contract deployed to address: ", contractInstance.address);
     console.log("-- Deploy account balance: ", ethers.utils.formatEther(await deployer.getBalance()));
     console.log("-- Deploy finished");
@@ -19,21 +21,44 @@ before(async function() {
 
 describe("Deploy test", async function() {
     it("Contract should be deployed successfully", async function() {
-        expect(contractInstance).to.be.ok;
+        expect(await contractInstance).to.be.ok;
     });
 
-    it("Version of the contract should be 1.0.0", async function() {
+    it("Contract version should be 1.0.0", async function() {
         const contractVersion = await contractInstance.getVersion();
         expect(contractVersion).to.be.equal("1.0.0");
     });
+
+    it("Contract should have one founder with contract creators address", async function() {
+        const counderAddress = await contractInstance.founder();
+        expect(counderAddress).to.be.equal(deployer.address);
+    });
+
+    it("Contract should have one owner (the founder)", async function() {
+        const owners = await contractInstance.getOwners();
+
+        expect(owners.length).to.be.equal(1);
+        expect(owners[0]).to.be.equal(deployer.address);
+    });
 });
 
-describe("Set value test", async function() {
-    it("Set myVariable to 20", async function() {
-        const tsx = await contractInstance.setMyVariable(20);
-        await tsx.wait();
-        const contractMyVariable = parseInt(await contractInstance.myVariable());
 
-        expect(contractMyVariable).to.be.equal(20);
+describe("Add an owner", async function() {
+    describe("When sender is an Owner", async function() {
+        it("should allow it", async function() {
+            await contractInstance.addOwner("0x1928feBA7284b967034D462691df508793DB9edD");
+
+            const isOwner = await contractInstance.isOwner("0x1928feBA7284b967034D462691df508793DB9edD");
+            const owners = await contractInstance.getOwners();
+            assert(isOwner, 'Not an owner');
+            expect(owners.length).to.be.equal(2);
+        });
     });
+
+    /*describe("When sender is NOT an Owner", async function() {
+        it("should NOT allow it", async function() {
+            // Como pruebo los requires?
+            expect(await contractInstance).to.be.ok;
+        });
+    });*/
 });
