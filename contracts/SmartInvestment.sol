6eb566c1d90a0fc5857e09a817c18a471b61686d
marto1712 <martin.trindade@gmail.com>
2021-11-09 22:45:41 -0300

    modifier que controla makers y auditores minimos para crear propuesta

--- contracts/SmartInvestment.sol
+++ contracts/SmartInvestment.sol
@@ -9,6 +9,9 @@ contract SmartInvestment {
     // State variables
     address public founder;
     address[] private _owners;
+    address[] private _auditors;
+    address[] private _voters;
+    address[] private _makers;
     Proposal[] private _proposals;
 
     // Mappings
@@ -26,10 +29,20 @@ contract SmartInvestment {
     // Events
     event founderSet(address indexed newFounder);
     event newOwner(address indexed addedBy, address indexed newOwner);
+    event newAuditor(address indexed addedBy, address indexed newAuditor);
+    event newVoter(address indexed addedBy, address indexed newVoter);
+    event newMaker(address indexed addedBy, address indexed newMaker);
 
     // Modifiers
     modifier onlyOwners() {
         require(_addressByRole[msg.sender][Account.Role.OWNER] == true, "Not an owner.");
+        _;
+    }
+
+    modifier isAvailableForProposalAndVote() {
+        uint256 auditorsCount = getRoleCount(_auditors, Account.Role.AUDITOR);
+        uint256 makersCount = getRoleCount(_makers, Account.Role.MAKER);
+        require(auditorsCount >= 2 && makersCount >= 3, "Not availabel for proposal and vote.");
         _;
     }
 
@@ -77,6 +90,33 @@ contract SmartInvestment {
         emit newOwner(msg.sender, _newOwnerAddress);
     }
 
+    function addAuditor(address _newAuditorAddress) external {
+        require(_newAuditorAddress != address(0), 'ERC20: approve from the zero address');
+        require(_addressByRole[_newAuditorAddress][Account.Role.AUDITOR] == false, 'Auditor already exists.');
+
+        _addressByRole[_newAuditorAddress][Account.Role.AUDITOR] = true;
+        _auditors.push(_newAuditorAddress);
+        emit newAuditor(msg.sender, _newAuditorAddress);
+    }
+
+    function addMaker(address _newMakerAddress) external {
+        require(_newMakerAddress != address(0), 'ERC20: approve from the zero address');
+        require(_addressByRole[_newMakerAddress][Account.Role.MAKER] == false, 'Maker already exists.');
+
+        _addressByRole[_newMakerAddress][Account.Role.MAKER] = true;
+        _makers.push(_newMakerAddress);
+        emit newMaker(msg.sender, _newMakerAddress);
+    }
+
+    function addVoter(address _newVoterAddress) external {
+        require(_newVoterAddress != address(0), 'ERC20: approve from the zero address');
+        require(_addressByRole[_newVoterAddress][Account.Role.VOTER] == false, 'Voter already exists.');
+
+        _addressByRole[_newVoterAddress][Account.Role.VOTER] = true;
+        _voters.push(_newVoterAddress);
+        emit newVoter(msg.sender, _newVoterAddress);
+    }
+
     // <view> porque va a leer de la blockchain.
     // No <external> porque puede ser leeido desde adentro del contrato tambien.
     function getProposalsCount() public view returns(uint256) {
@@ -87,13 +127,24 @@ contract SmartInvestment {
         return _proposals;
     }
 
+    function getRoleCount(address[] memory _addresses, Account.Role _role) public view returns(uint256) {
+        uint addressesLength = _addresses.length;
+        uint count = 0;
+        for (uint i=0; i < addressesLength; i++) {
+           if (_addressByRole[_addresses[i]][_role]) {
+            count ++;
+           }
+        }
+        return count;
+    }
+
     // function openProposalSubmissionPeriod
 
     // function closeProposalSubmissionPeriod {
         // openVotingPeriod
     // }
     
-    // function submitProposal() 
+    // function submitProposal() isAvailableForProposalAndVote()
 
     // 
 }
