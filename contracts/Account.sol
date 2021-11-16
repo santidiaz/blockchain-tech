9f52fd0a692b283e0f43daf3e8c32eb505888f6f
Santiago Diaz <santidiaz.uy@gmail.com>
2021-11-15 23:42:24 -0300

    Cambios en Account, creo que no lo necesitamos tampoco

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,27 +3,31 @@ pragma solidity 0.8.4;
 
 contract Account {
     // State variables
-    Role public role;
 
     // Mappings
-    
+    mapping(Role => MemberData) private _roles;
+
     // Enums
     enum Role { OWNER, MAKER, VOTER, AUDITOR }
     
     // Structs
+    struct MemberData {
+        mapping(address => bool) members;
+        Role role;
+    }
 
     // Address
-    address public ownerAddress;
+    // address public ownerAddress;
 
     // Events
     event transferToken(address indexed _from, address indexed _to, uint256 _amount);
     event accountSet(address indexed _from, address indexed _to, uint256 _role);
     
     // Modifiers
-    modifier onlyOwner() {
+    /*modifier onlyOwner() {
         require(msg.sender == ownerAddress, "Not the owner.");
         _;
-    }
+    }*/
 
     modifier balance(uint256 _amount) {
         require(address(this).balance >= _amount);
@@ -31,11 +35,11 @@ contract Account {
     }
 
     // Constructor
-    constructor(address _ownerAddress, Role _role) {
+    /*constructor(address _ownerAddress, Role _role) {
         ownerAddress = _ownerAddress;
         role = _role;
         emit accountSet(address(0), ownerAddress, uint256(_role));
-    }
+    }*/
     
     // Functions
     function getVersion() public pure returns(string memory) {
@@ -46,6 +50,11 @@ contract Account {
     function getBalance() external view returns(uint256) {
         // Otro ejemplo: owner.balance; Q: <this> hace referencia al <owner>? son lo mismo?
         return address(this).balance;
+    }
+    
+    
+    function hasRole(Role _role, address _account) private view returns (bool) {
+        return _roles[_role].members[_account];
     }
 
     receive() external payable {
